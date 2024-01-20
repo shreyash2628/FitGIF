cbf34e4775e07fd2389583148d528f1b63c123c5
Shreyash Pawar <shreyash.pawar26@gmail.com>
2024-01-21 00:52:35 +0530

    Started working on Create Workout Plan Feature

--- fitgif/src/Components/WorkoutPlan.jsx
+++ fitgif/src/Components/WorkoutPlan.jsx
@@ -1,15 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react';
+import downicon from '../Assets/down.png';
 
 const WorkoutPlan = () => {
+    const[showMuscleDropDown,setShowMuscleDropDown] = useState(false);
+
+    const handleOnDownArrowClickForSelectMuscle = ()=>{
+        console.log("hi");
+    }
+
   return (
     <div className='p-5 border border-4 w-screen h-screen bg-slate-800'>
             <div className='bg-white h-screen flex flex-col'>
                 <h1 className='flex justify-center pt-2'>Create Workout Plan</h1>
                 
-                <div className='flex flex-row '>
-                    <h1>hey</h1>
-                    <h1>helllo</h1>
-                </div>
+               <div className='bg-white w-56 h-6 m-2 flex flex-row'>
+                        <input className=' px-2 w-28 ' placeholder='select muscle'>
+                        </input>
+                        <img src={downicon} className='w-5  bg-white' alt="" onClick={handleOnDownArrowClickForSelectMuscle} />
+               </div>
 
             </div>
     </div>
